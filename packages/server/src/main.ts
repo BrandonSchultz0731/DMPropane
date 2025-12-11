cb1b4280f81bf648ca1e0d8671c1dc0c18b7a189
Brandon Schultz <brandonalexschultz@gmail.com>
2025-12-11 14:22:25 -0700

    push change to cause deploy

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -5,7 +5,6 @@ import cookieParser from "cookie-parser";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Enable CORS
   app.enableCors({
     origin: [process.env.CLIENT_URL, 'https://www.dmpropane.railway.internal', 'https://up.railway.app'],
     credentials: true,
